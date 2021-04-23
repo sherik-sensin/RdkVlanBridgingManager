df5e8ee70ee6afcdfa553d38f54b34b0c0ff1383
Arunkumar Sivadasan <arunkumar.sivadasan@sky.uk>
2021-04-23 14:54:22 +0000

    RDKB-35350 : Change VLAN Manager to use Sky license headers
    Reason for change: Added SKY license headers in all files
    Test Procedure: Check VLAN Manager is building without failure
    Risks: None
    
    Change-Id: Ic7e13a11e2627a9afd3682618862d90f95d30ce1
    Signed-off-by: Arunkumar Sivadasan <arunkumar.sivadasan@sky.uk>
    (cherry picked from commit 8aad4225e156cfb5a3616ac0dac0f7b00f9d3876)

--- source/TR-181/middle_layer_src/vlan_eth_hal.h
+++ source/TR-181/middle_layer_src/vlan_eth_hal.h
@@ -1,22 +1,32 @@
-/**
-* @file vlan_eth_hal.h
-* @brief For CCSP Component:  VLAN Termination HAL Layer
-*
-* @description This file gives the function prototypes used for the RDK-Broadband
-* Ethernet VLAN abstraction layer
-*/
-
 /*
- * If not stated otherwise in this file or this component's Licenses.txt file the
- * following copyright and licenses apply:
+ * Copyright (C) 2020 Sky
+ * --------------------------------------------------------------------------
+ * THIS SOFTWARE CONTRIBUTION IS PROVIDED ON BEHALF OF SKY PLC.
+ * BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED
+ * ******************************************************************
  *
- * Copyright 2019 RDK Management
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+/*
+ * Copyright [2014] [Cisco Systems, Inc.]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,7 +34,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-
 /**********************************************************************
 
     module: vlan_eth_hal.h
