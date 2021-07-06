dc509057bd7f837df94be4781838e04f8b87e805
Gautham Durairaj <gautham.durairaj@sky.uk>
2021-07-06 18:58:19 +0000

    RDKB-36181 : Compliance Cleanup in RdkVlanBridgingManager
    Reason for change: Compliance Cleanup in RdkVlanBridgingManager
    Test Procedure:
    	* Jenkins build should succeed
    Risks: Moderate
    Signed-off-by: gautham.durairaj@sky.uk
    Change-Id: I13a9905f5b475677ea8ba482ff96bd842a7a9dc4

--- source/TR-181/middle_layer_src/ethernet_dml.h
+++ source/TR-181/middle_layer_src/ethernet_dml.h
@@ -1,24 +1,21 @@
 /*
- * Copyright (C) 2020 Sky
- * --------------------------------------------------------------------------
- * THIS SOFTWARE CONTRIBUTION IS PROVIDED ON BEHALF OF SKY PLC.
- * BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
- * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED
- * ******************************************************************
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright 2020 Sky
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
-*/
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * Copyright [2014] [Cisco Systems, Inc.]
  *
